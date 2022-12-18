967e482aad8841e4d4594ef523ca856a043de970
NazirErgashiy <pikobiznes@gmail.com>
2022-12-18 16:05:56 +0500

    FileReader is ready for use

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "", age = "", email = "", phone = "";
+        try (java.io.FileReader fileReader = new java.io.FileReader(file)) {
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String str;
+            int i = 0;
+            while ((str = bufferedReader.readLine()) != null) {
+                switch (i) {
+                    case 0:
+                        name = str;
+                        break;
+                    case 1:
+                        age = str;
+                        break;
+                    case 2:
+                        email = str;
+                        break;
+                    case 3:
+                        phone = str;
+                        break;
+                }
+                i++;
+            }
+            bufferedReader.close();
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        name = name.replaceAll("Name: ", "");
+        age = age.replaceAll("Age: ", "");
+        email = email.replaceAll("Email: ", "");
+        phone = phone.replaceAll("Phone: ", "");
+
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
     }
 }
